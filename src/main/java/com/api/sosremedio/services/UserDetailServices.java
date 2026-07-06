d9fa5d709e748e92625318f26f1f13be33bc4bc1
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-07-06 20:17:45 -0300

    UserDetail: carregar usuarios pelo email

--- src/main/java/com/api/sosremedio/services/UserDetailServices.java
+++ src/main/java/com/api/sosremedio/services/UserDetailServices.java
@@ -1,4 +1,21 @@
 package com.api.sosremedio.services;
 
+import com.api.sosremedio.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor //gera construtor para final's
 public class UserDetailServices {
+    private final UserRepository userRepository;
+
+    //encontra e devolve o UserModel que implementa o userDetails
+    public UserDetails loadUserByUsername(String username) {
+        String normalizedEmail = username.trim().toLowerCase();
+
+        return userRepository.findByEmail(normalizedEmail).orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + normalizedEmail));
+    }
+
 }

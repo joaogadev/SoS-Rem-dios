d3afc7ce98eb642c92fb2bd08b583208ab3bf92e
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-07-09 16:36:06 -0300

    register e test funcionam

--- src/main/java/com/api/sosremedio/controller/AuthController.java
+++ src/main/java/com/api/sosremedio/controller/AuthController.java
@@ -1,4 +1,29 @@
 package com.api.sosremedio.controller;
 
+import com.api.sosremedio.DTO.RegisterRequest;
+import com.api.sosremedio.DTO.UserResponse;
+import com.api.sosremedio.services.AuthServices;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthServices authServices;
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
+        UserResponse user = authServices.register(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Auth público funcionando");
+    }
+
 }
